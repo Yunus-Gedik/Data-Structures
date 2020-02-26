030fd30310b04ae9dae62d17006bf1142abc8458
Yunus-Gedik <yunus-gedik@outlook.com>
2020-02-26 19:49:48 +0300

    Println düzenleme

--- HW-2/source/Main.java
+++ HW-2/source/Main.java
@@ -15,7 +15,7 @@ public class Main {
         String time = "timeInfo";
         for(int i=0; i<20; i++)
         {
-            System.out.println("----------------------");
+            System.out.println("--------------------------");
             day =  generator.nextInt(4) ;
             String setup = "setup"+Integer.toString(i);
             acc = (float) (i*0.1);
